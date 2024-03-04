790de06bcdfe02263c3e954434f6cbdb72095f4d
Ramiroqr <ramiroquisper@gmail.com>
2024-03-04 12:15:03 -0500

    getting categories

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,16 +1,28 @@
 import Image from 'next/image';
+import useQuiosco from '@/hooks/useQuiosco';
+import Categoria from './Categoria';
 
 const Sidebar = () => {
+  const { categorias } = useQuiosco();
+
   return (
-    <div>
+    <>
         <Image 
             width={150}
             height={100}
             src="/assets/img/logo.svg"
             alt='imagen logo'
         />
-    </div>
+        <nav className='mt-10'>
+          {categorias.map(categoria => (
+            <Categoria 
+              key={categoria.id}
+              categoria={categoria}
+            />
+          ))}
+        </nav>
+    </>
   )
-  }
+}
 
 export default Sidebar;
--- context/QuioscoProvider.jsx
+++ context/QuioscoProvider.jsx
@@ -1,13 +1,24 @@
+import axios from 'axios';
 import { useState, useEffect, createContext } from 'react';
 
 const QuioscoContext = createContext();
 
 const QuioscoProvider = ({ children }) =>  {
+    const [categorias, setCategorias] = useState([]);
+
+    const obtenerCategorias = async() => {
+        const { data } = await axios('/api/categorias');
+        setCategorias(data);
+    }
+
+    useEffect(() => {
+        obtenerCategorias();
+    }, [])
 
     return( 
         <QuioscoContext.Provider
             value={{
-
+                categorias,
             }}
         >
             {children}
@@ -19,4 +30,4 @@ export {
     QuioscoProvider
 }
 
-export default QuioscoContext
+export default QuioscoContext;
